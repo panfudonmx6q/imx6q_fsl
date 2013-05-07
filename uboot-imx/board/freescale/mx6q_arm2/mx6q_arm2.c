a4575caf7f56305f05f2ee4612acc97b40755864
dong <ppffdd@163.com>
2013-05-07 11:45:27 +0800

    DDR update freq

--- uboot-imx/board/freescale/mx6q_arm2/mx6q_arm2.c
+++ uboot-imx/board/freescale/mx6q_arm2/mx6q_arm2.c
@@ -312,10 +312,15 @@ static void setup_uart(void)
 {
 #if defined CONFIG_MX6Q
 	/* UART4 TXD */
-	mxc_iomux_v3_setup_pad(MX6Q_PAD_KEY_COL0__UART4_TXD);
+//	mxc_iomux_v3_setup_pad(MX6Q_PAD_KEY_COL0__UART4_TXD);
 
 	/* UART4 RXD */
-	mxc_iomux_v3_setup_pad(MX6Q_PAD_KEY_ROW0__UART4_RXD);
+//	mxc_iomux_v3_setup_pad(MX6Q_PAD_KEY_ROW0__UART4_RXD);
+	/* UART1 TXD */
+	mxc_iomux_v3_setup_pad(MX6Q_PAD_CSI0_DAT10__UART1_TXD);
+
+	/* UART1 RXD */
+	mxc_iomux_v3_setup_pad(MX6Q_PAD_CSI0_DAT11__UART1_RXD);
 #elif defined CONFIG_MX6DL
 	/* UART4 TXD */
 	mxc_iomux_v3_setup_pad(MX6DL_PAD_KEY_COL0__UART4_TXD);
