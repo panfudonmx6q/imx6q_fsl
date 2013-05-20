47d6716963285473d24daed4bf15cc06b8774c42
dong <ppffdd@163.com>
2013-05-20 15:15:20 +0800

    modified 1 evn.sh 2 rebuild.sh

--- kernel_imx/evn.sh
+++ kernel_imx/evn.sh
@@ -2,3 +2,4 @@
 export PATH=~/imx6q_android/bootable/bootloader/uboot-imx/tools:$PATH
 export ARCH=arm
 export CROSS_COMPILE=~/imx6q_android/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
+export LINUXDIR=~/imx6q_fsl/kernel_imx
--- kernel_imx/rebuild.sh
+++ kernel_imx/rebuild.sh
@@ -1,4 +1,5 @@
 
 make mrproper
 make imx6_android_defconfig
+#imx6_updater_defconfig
 make uImage
